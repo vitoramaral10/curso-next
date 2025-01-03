5a26a13cd284afe94a1434e4f708bbeda6ea70d5
Vitor Melo <vitor.melo@cmsw.com>
2025-01-03 14:16:34 -0300

    feat: add global styles, footer component, and update homepage structure

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,28 @@
 import Link from "../src/components/Link";
+import Footer from "../src/components/patterns/Footer";
+import React from "react";
+
+function Title({ children, as }) {
+  const Tag = as || "h1";
+
+  return (
+    <React.Fragment>
+      <Tag>{children}</Tag>
+      <style jsx>{`
+        ${Tag} {
+          color: red;
+        }
+      `}</style>
+    </React.Fragment>
+  );
+}
 
 export default function HomePage() {
   return (
     <div>
-      <h1>Alura Cases - Home Page</h1>
+      <Title as="h2">Alura Cases - Home Page</Title>
       <Link href="/faq">Ir para o FAQ</Link>
+      <Footer />
     </div>
   );
 }
